9a3e8a2db4f3ef843b7d01e61f87f7b2051abea9
Ubuntu <ubuntu@ip-172-31-4-58.ap-south-1.compute.internal>
2023-06-28 11:52:36 +0000

    modified main

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_virtual_machine" "test" {
-  name                  = "test"
+  name                  = "test_vm"
   location              = "us-east-1"
   resource_group_name   = "mytestrg"
 }
